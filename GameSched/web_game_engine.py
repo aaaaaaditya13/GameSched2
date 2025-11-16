a2331b492a1eaa3ffef84a3daf8a12b485c0fb0b
Aditya Singh <adity04thapa@gmail.com>
2025-11-16 11:44:21 +0530

    6th

--- GameSched/web_game_engine.py
+++ GameSched/web_game_engine.py
@@ -83,6 +83,9 @@ class WebScheduler:
         if 0 <= index < len(algorithms):
             self.scheduler = algorithms[index]
             self.current_algorithm_name = self.scheduler['name']
+            # Apply 3-second timer for all non-FCFS algorithms
+            if self.scheduler['type'] != 'fcfs':
+                self.powerup_algorithm_timer = self.powerup_algorithm_duration
     
     def apply_powerup_algorithm(self, algorithm_name):
         """Apply new scheduling algorithm from power-up for 3 seconds"""
@@ -470,6 +473,10 @@ class WebLineCrossingGame:
                     self.attempts += 1
                 else:
                     self.reset_positions()
+                    # Give player first move priority
+                    self.scheduler.ready_queue.clear()
+                    self.scheduler.running_process = None
+                    self.scheduler.add_process(self.player, 'movement')
                 return
         
         # Check collision with keys
